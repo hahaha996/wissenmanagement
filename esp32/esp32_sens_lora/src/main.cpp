a40ecafaafb9bc60c514d29ef1f1ad1e9e1eeb4b
hakhonglo <phamnganha96@gmail.com>
2024-01-21 21:06:02 +0100

    add comments

--- esp32/esp32_sens_lora/src/main.cpp
+++ esp32/esp32_sens_lora/src/main.cpp
@@ -5,13 +5,13 @@
 #include "DHT.h"
 
 // LORA: define the pins used by the transceiver module
-#define ss 5
-#define rst 14
-#define dio0 2
-// SENSOR:
+#define ss 5 // Chip select pin for LoRa
+#define rst 14 // Reset pin for LoRa
+#define dio0 2 // Interrupt pin for LoRa
+// DHT sensor configuration
 #define DHTPIN 25     // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-DHT dht(DHTPIN, DHTTYPE);
+#define DHTTYPE DHT22   // Type of DHT sensor DHT 22  (AM2302), AM2321
+DHT dht(DHTPIN, DHTTYPE); // DHT sensor instance
 // Array of locations
 String locations[] = {"Lichtenberg", "Mitte", "Spandau", "Pankow"};
 
@@ -30,6 +30,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // Humidity reading
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
@@ -53,25 +54,25 @@ void loop() {
   sprintf(resultString, "%d,%s,%.2f,%.2f,%.2f", counter, locations[randomIndex].c_str(), h+randomIndex*2, t+randomIndex*2, hic+randomIndex*2);
 
   // sprintf(resultString, "%d,Lichtenberg,%.2f,%.2f,%.2f", counter, h, t, hic);
-  Serial.println(resultString);
+  Serial.println(resultString); // Print the result string to the serial monitor.
+  // The serial monitor allows for viewing output from the ESP32, that is also monitor function in platformio.
 
-
-  LoRa.setPins(ss, rst, dio0);
+  LoRa.setPins(ss, rst, dio0); // Set LoRa module pins
   Serial.println("initing lora ... ");
-  while (!LoRa.begin(866E6))
+  while (!LoRa.begin(866E6)) // Initialize LoRa at 866 MHz, a frequency suitable for LoRa communication in Europe.
   {
     Serial.println(".");
     delay(500);
   }
-  LoRa.setSyncWord(0xF3);
+  LoRa.setSyncWord(0xF3); // Set synchronization word for LoRa communication
   delay(500);
   Serial.println("Sending msg: ...");
-  Serial.println(resultString);
-  LoRa.beginPacket();
-  LoRa.printf(resultString);
-  LoRa.endPacket();
+  Serial.println(resultString); // Print the message to be sent
+  LoRa.beginPacket(); // Start a new LoRa packet
+  LoRa.printf(resultString);  // Add data to the LoRa packet
+  LoRa.endPacket(); // Finish and send the LoRa packet
   counter = counter + 1;
   Serial.println("Sent. Sleep.");
-  delay(10000);
+  delay(10000); // Delay for 10 seconds before the next loop iteration
 
 }
